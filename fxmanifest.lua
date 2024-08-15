ad33dbcd95b868d33b43e4b1c716cd2a59f49507
Orhan Özeyranoğlu <13704525+qubitrepos@users.noreply.github.com>
2024-08-15 09:39:47 +0300

    initial commit 1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+games {"gta5"}
+
+description "Qubit's Subtitle"
+version '1.0.0'
+
+client_scripts {
+    'main.lua'
+}
+
+ui_page('html/index.html')
+
+files {
+    'html/listener.js',
+    'html/style.css',
+    'html/index.html',
+}
